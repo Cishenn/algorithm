5c3471f72a87341c496c79c509a5290e08642799
Cishenn Lee <740804059@qq.com>
2021-04-15 23:27:50 +0800

    add leetcode rob-dp

---
+++ leetcode/rob-dp.js
@@ -0,0 +1,20 @@
+const rob = function (nums) {
+    const len = nums.length;
+    if (len === 1) {
+        return nums[0];
+    }
+    else if (len === 2) {
+        return Math.max(nums[0],nums[1]);
+    }
+    return Math.max(robRange(nums, 0, len - 2), robRange(nums, 1, len - 1));
+}
+
+const robRange = (nums, start, end) => {
+    let first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+    for (let i = start + 2; i <= end; i++) {
+        const temp = second;
+        second = Math.max(first + nums[i], second);
+        first = temp;
+    }
+    return second;
+}
--- leetcode/ugly-enforced.js
+++ leetcode/ugly-enforced.js
@@ -1,4 +1,4 @@
 //Sun Apr 11 01:20:59 CST 2021
 function ugly-enforced(){
-
+    
 }
